ea6a85ecad1060a5b8a1a1a9f22e17222f3c92ab
Sergant073 <swtopic-2020@yandex.ru>
2024-10-09 18:11:54 +0400

    first

---
+++ pip1/Module_2_3..py
@@ -0,0 +1,15 @@
+# from pip1.Module_2_3 import is_prime
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for num in n:
+    if num > 1:
+        for i in range (2, i):
+            if num % i == 0:
+                is_prime = False
+            primes.append()
+            not_primes.append()
+
+        print("Простые числа:", primes)
+        print("Не простые числа:", not_primes)
