54e4279c3c5500163c3d88766005a15ac09a0856
Sergant073 <swtopic-2020@yandex.ru>
2024-10-12 18:57:41 +0400

    ДЗ готово!

--- pip1/Module_2_5.py
+++ pip1/Module_2_5.py
@@ -4,7 +4,7 @@ def get_matrix (n, m, value):
         matrix.append([])
         for j in range(m):
             matrix[i].append(value)
-    print(matrix)
+    print(*matrix)
 get_matrix(2, 2, 10)
 get_matrix(3, 5, 42)
 get_matrix(4, 2, 13)
