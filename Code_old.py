21884e9ce5f7425814e484765fe94d052bbe787e
Sergant073 <swtopic-2020@yandex.ru>
2024-10-27 15:10:43 +0400

    ДЗ исправил под ввод чисел

--- Code_old.py
+++ Code_old.py
@@ -1,3 +1,4 @@
+
 def find_pairs(target):
     password = ''
 
@@ -7,6 +8,9 @@ def find_pairs(target):
                 password += str(i) + str(j)
     return password
 
-for target in range(3, 21):
-    pairs = find_pairs(target)
-    print(f"Уникальные пары для числа {target}: {pairs}")
+# for target in range(3, 21):
+#     pairs = find_pairs(target.int(input))
+#     print(f"Уникальные пары для числа {target}: {pairs}")
+n = int(input('Ведите число от 3 до 20: '))
+result = find_pairs(n)
+print('Пароль:', result)
